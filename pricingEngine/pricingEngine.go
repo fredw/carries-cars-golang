970dccd721d08cadc15cfa08a3431eccbe9bc34e
Frederico Wuerges Becker <fred.wuerges@gmail.com>
2021-10-12 09:28:54 +0200

    Implements reservation feature

--- pricingEngine/pricingEngine.go
+++ pricingEngine/pricingEngine.go
@@ -1,8 +1,9 @@
 package pricingEngine
 
 import (
-	"carries-cars.com/money"
 	"errors"
+
+	"carries-cars.com/money"
 )
 
 // UnverifiedDuration should be used when accepting input from untrusted sources (pretty much anywhere) in the model.
@@ -20,6 +21,19 @@ func CalculatePrice(pricePerMinute money.Money, duration Duration) money.Money {
 	durationInMinutes := float64(duration.DurationInMinutes())
 
 	return pricePerMinute.MultiplyAndRound(durationInMinutes)
+}
+
+const reservationTimeLimitInMinutes = 20
+
+func CalculateReservationPrice(duration Duration) money.Money {
+	durationInMinutes := float64(duration.DurationInMinutes())
+	extraTimeSurcharge := money.EUR(0)
+	if durationInMinutes > reservationTimeLimitInMinutes {
+		extraTimeSurcharge = money.EUR(9)
+		durationInMinutes -= reservationTimeLimitInMinutes
+	}
+
+	return extraTimeSurcharge.MultiplyAndRound(durationInMinutes)
 }
 
 type Duration interface {
